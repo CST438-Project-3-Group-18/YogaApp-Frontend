91d9f4491df9b808e898f0ee4287d787333f7e05
shancabi <scabi@csumb.edu>
2025-10-29 19:45:47 -0700

    added buttons

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,7 +4,7 @@
  * Randomize button
  * Save to collection button
  */
-import { Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 function HomePageScreen(){
     return(
@@ -19,20 +19,34 @@ function HomePageScreen(){
             <Text style={styles.subheader}>Find what's right for you today!</Text>
         </View>
 
+        {/* Randomize Button */}
+        <TouchableOpacity style={styles.randButton} onPress={() => console.log('Pressed!')}>
+          <Text style={styles.randButtonText}>New Pose!</Text>
+        </TouchableOpacity>
+
         <View style={styles.poseContainer}>
+          {/* Name - Hardcoded for now*/}
+          <Text style={styles.poseName}>Downward Dog</Text>
           {/* Image - Hardcoded for now*/}
           <Image style={styles.imageContainer}
           source={{
             uri: 'https://hmtnijillluuhbsrdjvq.supabase.co/storage/v1/object/public/poses/downward_dog.jpg',
           }}
           />
+        {/* Description - Hardcoded for now*/}  
+        <Text style={styles.desc}>A foundational pose that lengthens the spine and strengthens the arms and legs.</Text>
 
-        {/* Description - hardcoded for now*/}
-        <View style={styles.description}>
-            <Text style={styles.descriptionText}>Style: Yin</Text>
-            <Text style={styles.descriptionText}>Difficulty: Easy</Text>
+        {/* Style/Difficulty - hardcoded for now*/}
+        <View style={styles.level}>
+            <Text style={styles.levelText}>Style: Yin</Text>
+            <Text style={styles.levelText}>Difficulty: Easy</Text>
         </View>
     </View>
+
+    {/* Save Button */}
+        <TouchableOpacity style={styles.saveButton} onPress={() => console.log('Pressed!')}>
+          <Text style={styles.saveButtonText}>Save Pose to Collection</Text>
+        </TouchableOpacity>
     </View>
     )
 }
@@ -47,12 +61,26 @@ const styles = StyleSheet.create({
       alignSelf: 'center',
       width: 300,
       borderRadius: 20,
+      marginBottom: 20,
+    },
+    poseName: {
+      fontSize: 22,
+      fontWeight: 'bold',
+      color: '#7e86c4',
+      textAlign: 'center',
+      marginTop: 10,
+    },
+    desc: {
+      fontSize: 15,
+      color: '#7e86c4',
+      marginTop: 15,
+      paddingHorizontal: 10,
     },
     homeTitle:{
             fontSize: 30,
             fontWeight: 'bold',
             textAlign: 'center',
-            marginTop: 50,
+            marginTop: 30,
             color: '#7e86c4',
     },
     imageContainer:{
@@ -69,7 +97,7 @@ const styles = StyleSheet.create({
         marginTop: 5,
         marginBottom: 20,
     },
-    description:{
+    level:{
       marginTop: 20,
       marginBottom: 15,
       backgroundColor: '#fff',
@@ -80,9 +108,37 @@ const styles = StyleSheet.create({
       alignSelf: 'center',
       justifyContent: 'center'
     },
-    descriptionText: {
+    levelText: {
       fontStyle: 'italic',
       marginLeft: 15,
-    }
+    },
+    randButton: {
+      backgroundColor: '#f78ba4',
+      padding: 10,
+      marginBottom: 20,
+      borderRadius: 20,
+      width: 150,
+      alignSelf: 'center',
+    },
+    randButtonText: {
+      color: '#faf2f4',
+      fontWeight: 'bold',
+      fontSize: 18,
+      textAlign: 'center',
+    },
+    saveButton: {
+      backgroundColor: '#7e86c4',
+      padding: 10,
+      borderRadius: 20,
+      width: 250,
+      alignSelf: 'center',
+    },
+    saveButtonText: {
+      color: '#faf2f4',
+      fontWeight: 'bold',
+      textAlign: 'center',
+      padding: 5,
+      fontSize: 18,
+    },
     });
 export default HomePageScreen;
